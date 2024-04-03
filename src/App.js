b7428b6eaffdba2735ab008455d3f61f1a048f04
zion-off <ajz317@nyu.edu>
2024-04-03 16:05:08 +0400

    ads

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
-import React from 'react';
-import ToggleCircle from './component';
-
+import React, { useState, useEffect }  from "react";
+import ToggleCircle from "./component";
+import haadi from "./haadi.jpg";
+import zein from "./zein.jpg";
 
 const App = () => {
+  const [randomImage, setRandomImage] = useState(null);
+
+  useEffect(() => {
+    
+    const imagesArray = [haadi, zein];
+
+    
+    const randomIndex = Math.floor(Math.random() * imagesArray.length);
+
+    
+    setRandomImage(imagesArray[randomIndex]);
+  }, []);
+
   return (
     <div
       style={{
-        width: '100vw',
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        flexWrap: 'wrap',
-        gap: '5vh',
-        backgroundColor: 'black',
-      }}
-    >
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexWrap: "wrap",
+        gap: "5vh",
+        backgroundColor: "black",
+      }}>
       <ToggleCircle text="zion" />
       <ToggleCircle text="zein" />
       <ToggleCircle text="hind" />
@@ -25,6 +38,21 @@ const App = () => {
       <ToggleCircle text="yazi" />
       <ToggleCircle text="malak" />
       <ToggleCircle text="khadija" />
+
+      {randomImage && (
+        <img
+          src={randomImage}
+          alt="ad"
+          style={{
+            position: "absolute",
+            bottom: "10px",
+            left: "10px",
+            width: "20vw", 
+            height: "auto", 
+            borderRadius: "5px", 
+          }}
+        />
+      )}
     </div>
   );
 };
